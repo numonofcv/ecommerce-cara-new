c15a39004a6fe68d002820fe88ed1a48605b24bc
numonofcv <numonofcv@gmail.com>
2025-12-30 00:17:42 +0500

    add  run deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
-import App from "./App.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
+import App from './App.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ecommerce-cara-new">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
